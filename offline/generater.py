42693685d9a3741e0b64b4234e2fc338c47c8263
Watremons <z2331355763@sina.com>
2023-06-15 21:18:28 +0800

    ✨ feat(offline): add weights to edges

--- offline/generater.py
+++ offline/generater.py
@@ -9,7 +9,7 @@ import networkx as nx
 from utils import create_folder
 
 
-def generate_dataset(seed: int, node_num: int, neighbor_num: int, add_edge_probability: float, keyword_set_size: int):
+def generate_dataset(seed: int, keywords_per_vertex_num: int, all_keyword_num: int, node_num: int, neighbor_num: int, add_edge_probability: float):
     random.seed(seed)
     np.random.seed(seed)
     # 1. Generate a graph
@@ -44,25 +44,33 @@ def generate_dataset(seed: int, node_num: int, neighbor_num: int, add_edge_proba
     print('Max Degree: ', max(d for v, d in nx.degree(target_graph)))
     print('Min Degree: ', min(d for v, d in nx.degree(target_graph)))
 
-    # 2.2 Add the keyword set for each vertex
-    keyword_set = ["sports", "shopping", "programming", "study", "reading", "writing", "e-sports", "series", "traveling", "music"]
-    label_counter = [0 for _ in range(keyword_set_size)]
+    # 2.2. Add the keyword set to each vertex
+    keywords_set = range(0, all_keyword_num)
+    label_counter = [0 for _ in range(all_keyword_num)]
     for i in range(target_graph.number_of_nodes()):
-        keyword_num = np.random.randint(1, keyword_set_size+1)
-        keyword_idxs = random.sample(range(0, 10), keyword_num)
-        keywords = []
-        for idx in keyword_idxs:
-            keywords.append(keyword_set[idx])
-            label_counter[idx] += 1
+        keyword_num = np.random.randint(max(keywords_per_vertex_num-1, 0), keywords_per_vertex_num+2)  # the num is key_per ± 1
+        keywords = random.sample(keywords_set, keyword_num)
+        for keyword in keywords:
+            label_counter[keyword] += 1
         target_graph.nodes[i]['keywords'] = keywords
 
-    print([{keyword_set[i]: label_counter[i]} for i in range(keyword_set_size)])
+    # print([{keywords_set[i]: label_counter[i]} for i in range(all_keyword_num)])
 
-    # 3. Save the graph as ".gpickle.gz" into a folder
-    folder_name = os.path.join("dataset", '{0}-{1}-{2}'.format(node_num, edges_num, keyword_set_size))
+    # TODO: 2.3. Add the weight (propagation probability) to each edge
+    # WC model: pp(u, v) for an edge (u, v) is 1/d(v), where d(v) is the in-degree of v.
+    for i in range(target_graph.number_of_nodes()):
+        neighbors_num = len(list(target_graph.neighbors(i)))
+        for neighbor in target_graph.neighbors(i):
+            target_graph.edges[i, neighbor]["weight"] = 1 / neighbors_num
+            # print(target_graph.get_edge_data(i, neighbor))
+    # TRIVALENCY model: randomly select a probability from the set {0.1, 0.01, 0.001}
+
+    # 3. Save the graph as ".gpickle.gz" into a folder, name
+    folder_name = os.path.join("dataset", "manual", '{0}-{1}-{2}-{3}'.format(node_num, edges_num, all_keyword_num, keywords_per_vertex_num))
     create_folder(folder_name=folder_name)
     os.chdir(folder_name)  # Switch path to the folder
     nx.write_gpickle(target_graph, 'data_graph.gpickle.gz')
+    print(folder_name, 'data_graph.gpickle.gz', "saved successfully!")
 
 
 if __name__ == "__main__":
@@ -70,8 +78,9 @@ if __name__ == "__main__":
     seed = 2022
     generate_dataset(
         seed=seed,
-        node_num=1000000,
+        keywords_per_vertex_num=3,  # 1, 2, 3, 4, 5
+        all_keyword_num=1000,  # 500, 800, 1K, 2K, 5K
+        node_num=50000,  # 10K, 30K, 50K, 100K, 500K, 1M
         neighbor_num=5,
-        add_edge_probability=0.250185,
-        keyword_set_size=10
+        add_edge_probability=0.250185
     )
