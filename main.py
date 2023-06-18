17ae7ca165808f1360cbe385459ab2c1928da652
Watremons <z2331355763@sina.com>
2023-06-18 21:29:53 +0800

    ✨ feat(main): modify the branches

--- main.py
+++ main.py
@@ -8,22 +8,24 @@ from process import execute_online
 if __name__ == "__main__":
     stat = Statistics()
     args = args_parser()
+    print("Start offline pre-computation:")
     if not is_precomputed(args.input):
         data_graph = data_graph_read(args.input)
         data_graph, index_root = execute_offline(data_graph=data_graph)
         mid_graph_save(mid_data_graph=data_graph, index=index_root, dataset_path=args.input)
-    else:
-        mid_data_graph, index_root = mid_graph_read(dataset_path=args.input)
-        result_set = execute_online(
-            data_graph=mid_data_graph,
-            query_keyword_Q=[int(keyword) for keyword in args.keywords.split(",")],
-            query_support_k=args.support,
-            radius_r=args.radius,
-            threshold_theta=args.theta,
-            query_L=args.top,
-            index_root=index_root
-        )
-        stat.solver_result = [seed_community for (seed_community, _) in result_set]
-        print(stat.solver_result)
+
+    print("Start online processing:")
+    mid_data_graph, index_root = mid_graph_read(dataset_path=args.input)
+    result_set = execute_online(
+        data_graph=mid_data_graph,
+        query_keyword_Q=[int(keyword) for keyword in args.keywords.split(",")],
+        query_support_k=args.support,
+        radius_r=args.radius,
+        threshold_theta=args.theta,
+        query_L=args.top,
+        index_root=index_root
+    )
+    stat.solver_result = [seed_community for (seed_community, _) in result_set]
+    print(stat.solver_result)
 
 # start: 19:02:30
