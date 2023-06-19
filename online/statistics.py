1590f9cc51c6c72149e234e049e19d55bd34b6c7
Watremons <z2331355763@sina.com>
2023-06-19 16:50:46 +0800

    ✨ feat(online): add statistics

---
+++ online/statistics.py
@@ -0,0 +1,77 @@
+import os
+
+
+class Statistics:
+    """
+    Class:
+        A class which used for record the statistic data
+    Attributes:
+        input_file_name: the file name of the input cnf file
+        output_file_name: the file name of the output result file
+        output_stat_file_name: the file name of the statistic data file
+        solver_result: the result of SAT problem for the input(SAT or UNSAT)
+        edge_num: the number of graph edge
+        node_num: the number of graph node
+        start_timestamp: the timestamp of the beginning
+        finish_timestamp: the timestamp of the end
+
+    """
+    def __init__(self, input_file_folder: str) -> None:
+        """
+        Method:
+            Constructed Method
+        """
+        self.input_file_name = os.path.join(input_file_folder, "data_graph.gpickle.gz")
+        self.mid_file_name = os.path.join(input_file_folder, "mid_data_graph.gpickle.gz")
+        self.index_file_name = os.path.join(input_file_folder, "index.json")
+        self.output_stat_file_name = os.path.join(input_file_folder, "statistics.txt")
+
+        self.solver_result = []
+        input_info = input_file_folder.split('/')[-1].split('-')
+        self.node_num = input_info[0]
+        self.edge_num = input_info[1]
+        self.all_keywords_num = input_info[2]
+        self.keywords_per_vertex = input_info[3]
+
+        self.start_timestamp = 0
+        self.finish_timestamp = 0
+
+        self.select_greatest_entry_in_H_time = 0
+        self.leaf_node_traverse_time = 0
+        self.nonleaf_node_traverse_time = 0
+        self.compute_r_hop_time = 0
+        self.compute_k_truss_time = 0
+        self.compute_influential_score_time = 0
+        self.modify_result_set_time = 0
+
+    def generate_stat_result(self) -> str:
+        """
+        Method:
+            Return the statistic result as string
+        """
+        result = ""
+        result += "STATISTIC RESULT\n"
+        result += "-------------FILE INFO-------------\n"
+        result += "Input File: {}\n".format(self.input_file_name)
+        result += "Mid Data File: {}\n".format(self.mid_file_name)
+        result += "Index File: {}\n".format(self.index_file_name)
+        result += "Statistic File: {}\n".format(self.output_stat_file_name)
+        result += "\n"
+        result += "-------------SOLVER INFO-------------\n"
+        result += "Result: {}\n".format([result[1] for result in self.solver_result])
+        result += "Total Nodes: {}\n".format(self.node_num)
+        result += "Total Edges: {}\n".format(self.edge_num)
+        result += "All Keywords: {}\n".format(self.all_keywords_num)
+        result += "Keywords Per Vertex: {}\n".format(self.keywords_per_vertex)
+        result += "\n"
+        result += "-------------TIME INFO-------------\n"
+        result += "Started at: {} \tFinished at: {}\n".format(self.start_timestamp, self.finish_timestamp)
+        result += "Total time: {}\n".format(self.finish_timestamp - self.start_timestamp)
+        result += "Select Greatest Entry in Heap time: {}\n".format(self.select_greatest_entry_in_H_time)
+        result += "Leaf Node Traverse time: {}\n".format(self.leaf_node_traverse_time)
+        result += "NonLeaf Node Traverse time: {}\n".format(self.nonleaf_node_traverse_time)
+        result += "Compute R-Hop time: {}\n".format(self.compute_r_hop_time)
+        result += "Compute K-Truss time: {}\n".format(self.compute_k_truss_time)
+        result += "Compute Influential Score time: {}\n".format(self.compute_influential_score_time)
+        result += "Modify Result Set time: {}\n".format(self.modify_result_set_time)
+        return result
