36954d26d59f2d70901178c0b1f6328807f467ab
Watremons <z2331355763@sina.com>
2023-11-05 15:30:30 +0800

    ✨ feat(argparser): add params n and d

--- utils/argparser.py
+++ utils/argparser.py
@@ -4,14 +4,13 @@ import argparse
 def args_parser():
     parser = argparse.ArgumentParser()
     # federated arguments
-    parser.add_argument("-i", "--input", type=str, help="the path of input file")
-    parser.add_argument("-Q", "--keywords", type=str, help="a series of keywords separated commas, query keywords")
-    parser.add_argument("-k", "--support", type=int, help="an integer, the support of seed communities")
-    parser.add_argument("-r", "--radius", type=int, help="an integer, the maximum radius of seed communities")
-    parser.add_argument("-t", "--theta", type=float, help="a float, the influence threshold")
-    parser.add_argument("-L", "--top", type=int, help="an integer, the number of result seed communities")
-    # parser.add_argument('--weight_decay', type=float, default=1e-4, help="weight_decay (default: 1e-4)")
-    # parser.add_argument('--split', type=str, default='user', help="train-test split type, user or sample")
-
+    parser.add_argument("-i", "--input", type=str, required=True, help="the path of input file")
+    parser.add_argument("-Q", "--keywords", type=str, required=True, help="a series of keywords separated commas, query keywords")
+    parser.add_argument("-k", "--support", type=int, required=True, help="an integer, the support of seed communities")
+    parser.add_argument("-r", "--radius", type=int, required=True, help="an integer, the maximum radius of seed communities")
+    parser.add_argument("-t", "--theta", type=float, required=True, help="a float, the influence threshold")
+    parser.add_argument("-L", "--top", type=int, required=True, help="an integer, the number of result seed communities")
+    parser.add_argument("-d", "--diversity", action="store_true", help="use the diversity refinement")
+    parser.add_argument("-n", "--nlparam", type=int, default=1, help="a integer, the number of nL approximation greedy algorithm")
     args = parser.parse_args()
     return args
