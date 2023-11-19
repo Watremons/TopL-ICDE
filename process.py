9992d5724115e4992526a5b2bc3f89d83637a4c6
Watremons <z2331355763@sina.com>
2023-11-19 15:28:24 +0800

    🐞 fix(process): remove optimal

--- process.py
+++ process.py
@@ -12,7 +12,7 @@ R_MAX = 2
 PRE_THETA_LIST = [0.2]
 
 
-def is_pass_pruning_entry(entry: dict, radius: int, query_bv: int, query_support: int, theta_z: float, sigma_L: float, optimal_mode: bool):
+def is_pass_pruning_entry(entry: dict, radius: int, query_bv: int, query_support: int, theta_z: float, sigma_L: float):
     # {
     #     "P": index_node,
     #     "R": [{
@@ -28,9 +28,8 @@ def is_pass_pruning_entry(entry: dict, radius: int, query_bv: int, query_support
         return False
     if synopsis["ub_sup_r"] < query_support:  # (Index-Level) Support Pruning
         return False
-    if optimal_mode:
-        if synopsis["Inf_ub"][str(theta_z)] < sigma_L:  # (Index-Level) Influential Score Pruning
-            return False
+    if synopsis["Inf_ub"][str(theta_z)] < sigma_L:  # (Index-Level) Influential Score Pruning
+        return False
     return True
 
 
@@ -70,7 +69,6 @@ def execute_online(
     query_L: int,
     nlparam: int,
     index_root: list,
-    optimal_mode: bool,
     stat: Statistics
 ) -> (set, int):
     # 0. initialization:
@@ -95,6 +93,7 @@ def execute_online(
         heapq.heappush(max_heap_H, IndexEntry(key=(-1)*entry["R"][radius_r_idx]["Inf_ub"][str(theta_z)], index_N=entry, idx=idx))
     # 0.6 set the L to n*L
     query_nL = nlparam * query_L
+    print("query_nL", query_nL)
     vertex_pruning_counter = 0
     leaf_node_visit_counter = 0
     entry_pruning_counter = 0
@@ -115,7 +114,7 @@ def execute_online(
         for child_entry in now_entry.index_N['P']:
             if child_entry['T']:  # N is a leaf node with child entry made by vertex
                 leaf_node_start_timestamp = time.time()
-                if is_pass_pruning_entry(entry=child_entry, radius=radius_r_idx, query_bv=q_bv, query_support=query_support_k-2, theta_z=theta_z, sigma_L=sigma_L, optimal_mode=optimal_mode):  # check the community-level pruning
+                if is_pass_pruning_entry(entry=child_entry, radius=radius_r_idx, query_bv=q_bv, query_support=query_support_k-2, theta_z=theta_z, sigma_L=sigma_L):  # check the community-level pruning
                     compute_r_hop_start_timestamp = time.time()
                     # hop_v_i_r = nx.ego_graph(G=data_graph, n=child_entry["P"], radius=radius_r, center=True)
                     hop_v_i_r = compute_hop_v_r(graph=data_graph, node_v=child_entry["P"], radius=radius_r)
@@ -166,7 +165,7 @@ def execute_online(
                 stat.leaf_node_traverse_time += (time.time() - leaf_node_start_timestamp)
             else:  # N is non-leaf node with child entry made by index node
                 nonleaf_node_start_timestamp = time.time()
-                if is_pass_pruning_entry(entry=child_entry, radius=radius_r_idx, query_bv=q_bv, query_support=query_support_k-2, theta_z=theta_z, sigma_L=sigma_L, optimal_mode=optimal_mode):  # check the community-level pruning
+                if is_pass_pruning_entry(entry=child_entry, radius=radius_r_idx, query_bv=q_bv, query_support=query_support_k-2, theta_z=theta_z, sigma_L=sigma_L):  # check the community-level pruning
                     heapq.heappush(max_heap_H, IndexEntry((-1)*child_entry["R"][radius_r_idx]["Inf_ub"][str(theta_z)], child_entry, idx))
                     if len(child_entry["P"]) == 0 or child_entry["P"][0]["T"]:
                         leaf_node_visit_counter += 1
@@ -178,13 +177,13 @@ def execute_online(
     stat.vertex_pruning_counter = (data_graph.number_of_nodes() - vertex_pruning_counter)
     stat.entry_pruning_counter = entry_pruning_counter
     stat.leaf_node_visit_counter = leaf_node_visit_counter
-    print("Max k truss cost:", max_k_truss_cost)
-    print("Max score cost:", max_influential_score_cost)
+    # print("Max k truss cost:", max_k_truss_cost)
+    # print("Max score cost:", max_influential_score_cost)
 
     # print(data_graph.nodes(data=True))
     sum_influential_score = 0
     for seed_community in result_set_S:
-        # print(seed_community[0].nodes)
+        print(seed_community[0].nodes)
         sum_influential_score += seed_community[1]
 
     return result_set_S, sum_influential_score
